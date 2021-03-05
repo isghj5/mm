3b33aee4413cbd0d4586e0b747511022d9635ccf
isghj8 <isghj8@gmail.com>
2021-03-04 23:07:25 -0800

    init almost matches

--- src/overlays/actors/ovl_En_Niw/z_en_niw.c
+++ src/overlays/actors/ovl_En_Niw/z_en_niw.c
@@ -9,6 +9,10 @@ void EnNiw_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnNiw_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnNiw_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_80891D78(EnNiw* this, GlobalContext* globalCtx);
+
+u32 D_80893460 = 0x0; // padding?
+
 /*
 const ActorInit En_Niw_InitVars = {
     ACTOR_EN_NIW,
@@ -23,7 +27,115 @@ const ActorInit En_Niw_InitVars = {
 };
 */
 
-#pragma GLOBAL_ASM("asm/non_matchings/ovl_En_Niw_0x80891060/EnNiw_Init.asm")
+u32 D_80893484[] = {
+ 0x459C4000,
+ 0xC59C4000,
+};
+
+u32 D_8089348C = 0x459C4000; 
+
+u32 D_80893490[] = {
+ 0x453B8000,
+ 0x457A0000,
+};
+
+u32 D_80893498[] = { // collider
+ 0x0500093D,
+ 0x20010000,
+ 0x00000000,
+ 0x00000000,
+ 0x00000000,
+ 0xF7CFFFFF,
+ 0x00000000,
+ 0x00010100,
+ 0x000F0019,
+ 0x00040000,
+ 0x00000000,
+};
+
+Vec3f D_808934C4 = {
+ 90000.0f,
+ 90000.0f,
+ 90000.0f,
+};
+
+u32 D_808934D0[] = {
+ 0x801F0006,
+ 0xB874F830,
+ 0x30540000,
+};
+
+u32 D_808934DC[] = {
+ 0x47AFC800,
+ 0x47AFC800,
+ 0x47AFC800,
+};
+
+u32 D_808934E8[] = {
+ 0x47AFC800,
+ 0x47AFC800,
+ 0x47AFC800,
+ 0x00000000,
+ 0x00000000,
+ 0x00000000,
+};
+
+void EnNiw_Init(Actor *thisx, GlobalContext *globalCtx) {
+    // NON matching: one stack offset issue and struct is wrong
+
+    EnNiw* this = THIS;
+    PosRot *worldPosRotTemp;
+    Vec3f D_Temp = D_808934C4;
+
+    if ( this->actor.params < 0) { //no bit 16 I guess
+        this->actor.params = 0;
+    }
+
+
+    Math_Vec3f_Copy( &this->unk2BC, &D_Temp);
+
+
+    this->paramsCopy = this->actor.params;
+    Actor_ProcessInitChain(thisx, &D_808934D0);
+    thisx->flags |= 1;
+    Actor_SetDrawParams(&thisx->shape, 0.0f, func_800B3FC0, 25.0f);
+
+    SkelAnime_InitSV(globalCtx, &this->skelanime,  
+        &D_6002530, &D_60000E8,
+         &this->limbDrawTbl, &this->transitionDrawtable, 0x10);
+    worldPosRotTemp = &this->actor.world;
+    Math_Vec3f_Copy(&this->unk2A4, &worldPosRotTemp->pos);
+    //Math_Vec3f_Copy(&this->unk2B0, worldPosRotTemp.rot);
+    Math_Vec3f_Copy(&this->unk2B0, &worldPosRotTemp->pos);
+
+    this->unk308 = 10.0f;
+    Actor_SetScale(&this->actor, 0.01f);
+
+    if (this->paramsCopy == 1) {
+        Actor_SetScale(&this->actor, ((f32) gStaticContext->data[2486] / D_80893500) + D_80893504);
+    }
+    thisx->colChkInfo.health = (u8) (u32) (randZeroOneScaled(D_80893508) + 10.0f);
+    thisx->colChkInfo.mass = 0xFF;
+    if (this->paramsCopy == 0) {
+        Collision_InitCylinder(globalCtx, &this->collider, &this->actor, &D_80893498);
+    }
+    if (this->paramsCopy == 2) {
+        func_800B8EC8(thisx, 0x2813U);
+        this->unk256 = (u16)0x1E;
+        this->unk250 = (u16)0x1E;
+        thisx->flags &= ~1;
+        this->unk28E = (u16)4;
+        this->actionFunc = func_80891D78;
+        thisx->speedXZ = 0.0f;
+        this->unk2BC.z = 0.0f;
+        thisx->velocity.y = 0.0f;
+        thisx->gravity = 0.0f;
+        return;
+    }
+    func_80891974(thisx);
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/ovl_En_Niw_0x80891060/EnNiw_Init.asm")
 
 #pragma GLOBAL_ASM("asm/non_matchings/ovl_En_Niw_0x80891060/EnNiw_Destroy.asm")
 
--- src/overlays/actors/ovl_En_Niw/z_en_niw.h
+++ src/overlays/actors/ovl_En_Niw/z_en_niw.h
@@ -5,11 +5,57 @@
 
 struct EnNiw;
 
+typedef void (*EnNiwActionFunc)(struct EnNiw*, GlobalContext*);
+
 typedef struct EnNiw {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x678];
+    ///* 0x144 */ char unk_144[0x678];
+    /* 0x144 */ SkelAnime skelanime;
+    /* 0x188 */ Vec3s limbDrawTbl[0x10];
+    /* 0x1E8 */ Vec3s transitionDrawtable[0x10]; //16 in oot
+    
+    /* 0x248 */ EnNiwActionFunc* actionFunc;
+    /* 0x24C */ u16 unk24C;// used in func_808922D0
+    /* 0x250 */ u16 unk250;
+    /* 0x252 */ u16 unk252;
+    /* 0x254 */ u16 unk254;
+    /* 0x256 */ u16 unk256;
+    /* 0x258 */ s16 unk258;
+    /* 0x25A */ s16 unk25A;
+    /* 0x25C */ s16 unk25C;
+    /* 0x25E */ s16 unk25E;
+    /* 0x260 */ s16 unk260;
+    /* 0x262 */ s16 unk262;
+    /* 0x264 */ f32 unk264; // used in func_808922D0
+    /* 0x268 */ f32 unk268; // used in func_808922D0
+    /* 0x26C */ f32 unk26C; // used in func_808922D0
+    /* 0x278 */ f32 unk278; // used in func_808922D0
+    /* 0x27C */ f32 unk27C; // used in func_808922D0
+    /* 0x280 */ f32 unk280; // used in func_808922D0
+    /* 0x280 */ f32 unk284; // used in func_808922D0
+    /* 0x28C */ s16 unk28C; // used in EnNiw_Update
+    /* 0x28E */ u16 unk28E;
+    /* 0x290 */ s16 unk290;
+    /* 0x292 */ u16 unk292[0x5];
+    /* 0x2A2 */ s16 paramsCopy;
+    /* 0x2A4 */ Vec3f unk2A4; // this actors starting position
+    /* 0x2B0 */ Vec3f unk2B0;// this actors starting position
+    /* 0x2BC */ Vec3f unk2BC; // init
+    /* 0x2C8 */ u16 unk2C8[10];
+    /* 0x2E8 */ s16 unk2E8;
+    /* 0x2EC */ u16 unk2EC[0x8];
+    /* 0x308 */ f32 unk308; // init 10f
+    /* 0x30C */ s16 unk30C; //unused?
+    /* 0x310 */ ColCylinder collider;
 } EnNiw; // size = 0x7BC
 
 extern const ActorInit En_Niw_InitVars;
+
+extern SkeletonHeader D_6002530;
+extern AnimationHeader D_60000E8;
+
+extern f32 D_80893500;
+extern f32 D_80893504;
+extern f32 D_80893508;
 
 #endif // Z_EN_NIW_H
