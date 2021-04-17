ce36efb3415000f1b03412ea4896628ae75f98ee
isghj8 <isghj8@gmail.com>
2021-04-17 16:35:31 -0700

    EnMm: docs

--- src/overlays/actors/ovl_En_Mm/z_en_mm.c
+++ src/overlays/actors/ovl_En_Mm/z_en_mm.c
@@ -26,14 +26,12 @@ const ActorInit En_Mm_InitVars = {
     (ActorFunc)EnMm_Draw
 };
 
-//D_80966340[] = {
 static ColliderCylinderInit sCylinderInit = {
     { COLTYPE_METAL, AT_NONE, AC_NONE, OC1_ON | OC1_TYPE_ALL, OC2_TYPE_2, COLSHAPE_CYLINDER, },
     { ELEMTYPE_UNK2, { 0x00100000, 0x00, 0x00 }, { 0x01000202, 0x00, 0x00 }, TOUCH_NONE | TOUCH_SFX_NORMAL, BUMP_NONE, OCELEM_ON, },
     { 6, 30, 0, { 0, 0, 0 } },
 };
 
-//u32 D_8096636C[] = {
 static InitChainEntry sInitChain[] = {
     ICHAIN_F32_DIV1000(gravity, -1200, ICHAIN_CONTINUE),
     ICHAIN_VEC3F_DIV1000(scale, 270, ICHAIN_STOP),
@@ -44,7 +42,7 @@ void func_80965BB0(EnMm* this, EnMmActionFunc* newActionFunc) {
     this->actionFunc = newActionFunc;
 }
 
-void func_80965BBC(EnMm *this) {
+void func_80965BBC(EnMm* this) {
     func_80965BB0(this, &func_8096611C); //set actionfunc
     this->actor.room = -1;
     if (this->actor.parent->id == 0) {
@@ -52,55 +50,53 @@ void func_80965BBC(EnMm *this) {
     }
 }
 
-void EnMm_Init(Actor *thisx, GlobalContext *globalCtx) {
+void EnMm_Init(Actor* thisx, GlobalContext* globalCtx) {
     EnMm* this = THIS;
     EnMmActionFunc *actionFunc;
 
-    if (this->actor.params >= 0) {
-        if ((gSaveContext.perm.weekEventReg[0x25] & 0x10) == 0 ||
-           (gSaveContext.perm.weekEventReg[0x25] & 0x08) != 0 ||
-           gSaveContext.owl.unk8 != 0) {
+    if ((this->actor.params >= 0) && ((gSaveContext.perm.weekEventReg[0x25] & 0x10) == 0
+        || (gSaveContext.perm.weekEventReg[0x25] & 0x08) != 0 || gSaveContext.owl.unk8 != 0)) {
             Actor_MarkForDeath(&this->actor);
             return;
-        }
     }
 
     Actor_ProcessInitChain(&this->actor, &sInitChain);
-    Actor_SetDrawParams(&this->actor.shape, 50.0f, &func_800B3FC0, 1.2f);
+    //Actor_SetDrawParams(&this->actor.shape, 50.0f, &func_800B3FC0, 1.2f);
+    ActorShape_Init(&this->actor.shape, 50.0f, &func_800B3FC0, 1.2f);
     Collider_InitCylinder(globalCtx,  &this->collider);
     Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
 
     if (this->actor.parent != 0) {
         func_80965BBC(&this->actor);
-        return;
+    } else {
+        actionFunc = (this->actor.cutscene >= 0) ? func_80965D3C : func_80965DB4;
+        func_80965BB0(&this->actor, actionFunc);
     }
-    actionFunc = (this->actor.cutscene >= 0) ? func_80965D3C : func_80965DB4;
-    func_80965BB0(&this->actor, actionFunc); //set actionfunc
 }
 
-void EnMm_Destroy(Actor *thisx, GlobalContext *globalCtx) {
+void EnMm_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     EnMm* this = THIS;
     Collider_DestroyCylinder(globalCtx, &this->collider);
 }
 
 // action func
-void func_80965D3C(EnMm *this, GlobalContext *globalCtx) {
+void func_80965D3C(EnMm* this, GlobalContext* globalCtx) {
     s16 cutscene;
 
     cutscene = ActorCutscene_GetAdditionalCutscene( this->actor.cutscene);
     if (ActorCutscene_GetCanPlayNext(cutscene) != 0) {
         ActorCutscene_StartAndSetUnkLinkFields(cutscene, &this->actor);
-        func_80965BB0(&this->actor, func_80965DB4); //set actionfunc
-        return;
+        func_80965BB0(&this->actor, func_80965DB4);
+    } else {
+        ActorCutscene_SetIntentToPlay(cutscene);
     }
-    ActorCutscene_SetIntentToPlay(cutscene);
 }
 
+#if NON_EQUIVELENT
 // action func
 // think this is calculating speed/direction of sirloin when breaking pot
-#if NON-MATCHING
 // the math at the 0x8000 is wrong, other issues
-void func_80965DB4(EnMm *this, GlobalContext *globalCtx) {
+void func_80965DB4(EnMm* this, GlobalContext* globalCtx) {
     //const static f32 D_80966380[] = { -0.3, 0.0, 0.0, 0.0, };
     
     Vec3f sp50;
@@ -124,7 +120,6 @@ void func_80965DB4(EnMm *this, GlobalContext *globalCtx) {
         if ((this->actor.speedXZ != 0.0f) && ((this->actor.bgCheckFlags & 8) != 0)) {
             //overflowtemp = 0x8000;
             dRot = this->actor.wallYaw + 0x8000;
-            //if(overflowtemp){}
             //this->actor.world.rot.y += (s16)(0x8000 - (this->actor.world.rot.y - (this->actor.wallYaw + 0x8000)) * 2);
             this->actor.world.rot.y += (0x8000 - (this->actor.world.rot.y - dRot) * 2);
             //this->actor.world.rot.y += (overflowtemp - (s16)(this->actor.world.rot.y - (this->actor.wallYaw + overflowtemp)) * 2);
@@ -155,7 +150,8 @@ void func_80965DB4(EnMm *this, GlobalContext *globalCtx) {
                 }
 
                 // lots of temps loaded/added here that shouldn't be
-                this->actor.world.rot.y = atans_flip(sinsSpeed, cossSpeed);
+                //this->actor.world.rot.y = atans_flip(sinsSpeed, cossSpeed);
+                this->actor.world.rot.y = Math_FAtan2F(sinsSpeed, cossSpeed);
             }
 
             if (Lib_StepTowardsCheck_f(&this->actor.speedXZ, 0.0f, 1.0f) == 0) {
@@ -165,15 +161,17 @@ void func_80965DB4(EnMm *this, GlobalContext *globalCtx) {
                 } else {
                     dRot32 = (s32) dRot;
                 }
+
                 newYRot = this->actor.world.rot.y;
                 dummy_permuter_label_func_80965DB4: ; 
                 if (dRot32 >= 0x4001) {
                     newYRot = (newYRot + 0x8000);
                 }
+
                 Lib_StepTowardsCheckFramerateScaled_s(&this->actor.shape.rot.y,
                       newYRot, 
                       (s16) (this->actor.speedXZ * 100.0f));
-                this->unkDraw = this->unkDraw + (s32) (this->actor.speedXZ * 800.0f);
+                this->xRot += (s32) (this->actor.speedXZ * 800.0f);
             }
 
             if ((this->actor.bgCheckFlags & 2) != 0) {
@@ -199,7 +197,7 @@ void func_80965DB4(EnMm *this, GlobalContext *globalCtx) {
 #endif
 
 // actor func
-void func_8096611C(EnMm *this, GlobalContext *globalCtx) {
+void func_8096611C(EnMm* this, GlobalContext* globalCtx) {
     if (func_800B8BFC(&this->actor, globalCtx) != 0) {
         func_80965BB0(&this->actor, func_80965DB4); //set actionfunc
         this->actor.room = globalCtx->roomContext.currRoom.num;
@@ -209,10 +207,10 @@ void func_8096611C(EnMm *this, GlobalContext *globalCtx) {
     } else {
         Math_Vec3f_ToVec3s(&this->actor.home.rot, &this->actor.parent->world);
     }
-    Lib_StepTowardsCheckFramerateScaled_s(&this->unkDraw, 0, 0x7D0);
+    Lib_StepTowardsCheckFramerateScaled_s(&this->xRot, 0, 0x7D0);
 }
 
-void EnMm_Update(Actor *thisx, GlobalContext *globalCtx) {
+void EnMm_Update(Actor* thisx, GlobalContext* globalCtx) {
     EnMm* this = THIS;
 
     Collider_ResetCylinderAC(globalCtx, &this->collider );
@@ -223,7 +221,7 @@ void EnMm_Update(Actor *thisx, GlobalContext *globalCtx) {
 
 #if NON-MATCHING
 // stack offset
-void EnMm_Draw(Actor *thisx, GlobalContext *globalCtx) {
+void EnMm_Draw(Actor* thisx, GlobalContext* globalCtx) {
     EnMm* this = THIS;
     s8 pad[4];
     //s8 pad2[1];
@@ -233,14 +231,17 @@ void EnMm_Draw(Actor *thisx, GlobalContext *globalCtx) {
     OPEN_DISPS(globalCtx->state.gfxCtx);
 
     func_8012C28C(globalCtx->state.gfxCtx);
-    if (this->unkDraw != 0) {
+    if (this->xRot != 0) {
         dY = this->actor.world.rot.y - this->actor.shape.rot.y;
-        SysMatrix_InsertYRotation_s(dY, 1);
-        SysMatrix_InsertXRotation_s(this->unkDraw, 1);
-        SysMatrix_InsertYRotation_s((s16) -(s32) dY, 1);
+        //SysMatrix_InsertYRotation_s(dY, 1);
+        //SysMatrix_InsertXRotation_s(this->xRot, 1);
+        //SysMatrix_InsertYRotation_s((s16) -(s32) dY, 1);
+        Matrix_RotateY(dY, 1);
+        SysMatrix_InsertXRotation_s(this->xRot, 1);
+        Matrix_RotateY((s16) -(s32) dY, 1);
     }
     gSPMatrix(POLY_OPA_DISP++,
-      SysMatrix_AppendStateToPolyOpaDisp(globalCtx->state.gfxCtx), 
+      Matrix_NewMtx(globalCtx->state.gfxCtx),
       G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
 
     gSPDisplayList(POLY_OPA_DISP++, &D_04055628);
--- src/overlays/actors/ovl_En_Mm/z_en_mm.h
+++ src/overlays/actors/ovl_En_Mm/z_en_mm.h
@@ -11,13 +11,13 @@ typedef struct EnMm {
     /* 0x000 */ Actor actor;
     ///* 0x144 */ char unk_144[0x678];
     /* 0x144 */ ColliderCylinder collider;
-    /* 0x190 */ s16 unkDraw ;
+    /* 0x190 */ s16 xRot;
     /* 0x190 */ s16 padding ;
     /* 0x194 */ EnMmActionFunc actionFunc;
     
 } EnMm; // size = 0x198
 
-extern u32 D_04055628;//0x04055628
+extern Gfx D_04055628[]; // displaylist
 
 extern f32 D_80966380; // velocity modifier used in func_80965DB4
 
