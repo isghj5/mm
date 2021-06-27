c3d5f07e7817447c651e31876c843ad717650a46
isghj8 <isghj8@gmail.com>
2021-06-26 23:16:23 -0700

    EnLookNuts: starting

--- src/overlays/actors/ovl_En_Look_Nuts/z_en_look_nuts.c
+++ src/overlays/actors/ovl_En_Look_Nuts/z_en_look_nuts.c
@@ -6,8 +6,21 @@
 
 void EnLookNuts_Init(Actor* thisx, GlobalContext* globalCtx);
 void EnLookNuts_Destroy(Actor* thisx, GlobalContext* globalCtx);
-void EnLookNuts_Update(Actor* thisx, GlobalContext* globalCtx);
-void EnLookNuts_Draw(Actor* thisx, GlobalContext* globalCtx);
+//void EnLookNuts_Update(Actor* thisx, GlobalContext* globalCtx);
+//void EnLookNuts_Draw(Actor* thisx, GlobalContext* globalCtx);
+//void EnLookNuts_Init(EnLookNuts* this, GlobalContext* globalCtx);
+void EnLookNuts_Update(EnLookNuts* this, GlobalContext* globalCtx);
+void EnLookNuts_Draw(EnLookNuts* this, GlobalContext* globalCtx);
+
+void func_80A67A34(EnLookNuts* this);
+void func_80A67C48(EnLookNuts* this);
+void func_80A67F30(EnLookNuts* this);
+void func_80A68080(EnLookNuts* this);
+
+void func_80A67AA8(EnLookNuts* this, GlobalContext* globalCtx);
+void func_80A67D0C(EnLookNuts* this, GlobalContext* globalCtx);
+void func_80A67FC4(EnLookNuts* this, GlobalContext* globalCtx);
+void func_80A680FC(EnLookNuts* this, GlobalContext* globalCtx);
 
 /*
 const ActorInit En_Look_Nuts_InitVars = {
@@ -23,13 +36,105 @@ const ActorInit En_Look_Nuts_InitVars = {
 };
 */
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Look_Nuts_0x80A678B0/EnLookNuts_Init.asm")
+extern DamageTable D_80A68630;
+extern u32 D_80A68600[];
+extern u32 D_06002848[];
+extern u32 D_06000430[];
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Look_Nuts_0x80A678B0/EnLookNuts_Destroy.asm")
+void EnLookNuts_Init(Actor* thisx, GlobalContext *globalCtx) {
+    EnLookNuts* this = (EnLookNuts*)thisx;
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Look_Nuts_0x80A678B0/func_80A67A34.asm")
+    ActorShape_Init(&this->actor.shape, 0.0f, func_800B3FC0, 20.0f);
+    SkelAnime_Init(globalCtx, &this->skelAnime, &D_06002848, &D_06000430, this->limbDrawTbl, this->transitionDrawTable, LOOKNUTS_LIMB_COUNT);
+    Actor_SetScale(&this->actor, 0.01f); // default is huge?
+    this->actor.colChkInfo.damageTable = &D_80A68630; // why damage table ????
+    this->actor.colChkInfo.mass = 0xFF; //immovable? why? so you cant push them? they have no collider
+    this->actor.targetMode = 1;
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Look_Nuts_0x80A678B0/func_80A67AA8.asm")
+    Collider_InitAndSetCylinder(globalCtx, &this->collider, (Actor *) this, &D_80A68600);
+    this->actor.flags |= 0x8000000;
+    this->paramsCopy2 = (this->actor.params >> 7) & 0x1F;
+    this->switchFlag = this->actor.params & 0x7F;
+    this->unk226 = (this->actor.params >> 0xC) & 0xF;
+  
+    // all vanilla use 0x7F, so unused beta content?
+    if (this->switchFlag == 0x7F) {
+        this->switchFlag = -1;
+    }
+    if (this->switchFlag >= 0 && Flags_GetSwitch(globalCtx, this->switchFlag)) {
+        Actor_MarkForDeath((Actor *) this);
+    } else if (this->paramsCopy2 == 0x1F) {
+        Actor_MarkForDeath((Actor *) this);
+    } else {
+        this->unk21C = 0;
+        func_80A67A34(this);
+    }
+}
+
+void EnLookNuts_Destroy(Actor *thisx, GlobalContext *globalCtx) {
+    EnLookNuts* this = (EnLookNuts*)thisx;
+    Collider_DestroyCylinder(globalCtx, &this->collider);
+}
+
+extern u32 D_06002B6C[];
+
+void func_80A67A34(EnLookNuts *this) {
+    SkelAnime_ChangeAnim(&this->skelAnime, D_06002B6C, 1.0f, 0.0f, SkelAnime_GetFrameCount(D_06002B6C), 0, -10.0f);
+    this->unk21C = 0;
+    this->actionFunc = func_80A67AA8;
+}
+
+extern u32 D_80A6862C;
+
+void func_80A67AA8(EnLookNuts *this, GlobalContext *globalCtx) {
+    s32 sp34;
+    f32 sp30;
+    //Vec3f sp30;
+    //SkelAnime *sp2C;
+    //SkelAnime *temp_a0;
+    s8 *temp_v0;
+    s8 *temp_v0_2;
+
+    //temp_a0 = &this->skelAnime;
+    //sp2C = temp_a0;
+    //sp30.y = 0.0f;
+    sp34 = 0.0f;
+    SkelAnime_FrameUpdateMatrix(&this->skelAnime);
+    if (func_801690CC(globalCtx) != 0) {
+        this->actor.speedXZ = 0.0f;
+        return;
+    }
+    this->actor.speedXZ = 2.0f;
+    if ((func_801378B8(&this->skelAnime, 1.0f) != 0) || (func_801378B8(&this->skelAnime, 5.0f) != 0)) {
+        Audio_PlayActorSound2((Actor *) this, (u16)0x387FU);
+    }
+    if (D_80A6862C != 0) {
+        Math_ApproachZeroF(&this->actor.speedXZ, 0.3f, 1.0f);
+        return;
+    }
+    temp_v0 = func_8013D648(globalCtx, this->paramsCopy2, 0x1F);
+    this->unk210 = temp_v0;
+    if (temp_v0 != 0) {
+        //sp30.y = (f32) func_8013D83C(temp_v0, this->unk214, &this->actor.world, &sp30);
+        sp34 = (f32) func_8013D83C(temp_v0, this->unk214, &this->actor.world, sp30);
+    }
+    if (sp30 < 10.0f) {
+        temp_v0_2 = this->unk210;
+        if (temp_v0_2 != 0) {
+            this->unk214 = (s16) (this->unk214 + 1);
+            if ((s32) this->unk214 >= (s32) (u8) *temp_v0_2) {
+                this->unk214 = (u16)0;
+            }
+            if (Rand_ZeroOne() < 0.6f) {
+                func_80A67C48(this);
+                return;
+            }
+        }
+    }
+    Math_SmoothStepToS(&this->actor.shape.rot.y, (s16) (s32) sp34, 1, 0x1388, 0);
+    this->actor.world.rot.y = this->actor.shape.rot.y;
+}
+//#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Look_Nuts_0x80A678B0/func_80A67AA8.asm")
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Look_Nuts_0x80A678B0/func_80A67C48.asm")
 
--- src/overlays/actors/ovl_En_Look_Nuts/z_en_look_nuts.h
+++ src/overlays/actors/ovl_En_Look_Nuts/z_en_look_nuts.h
@@ -5,9 +5,30 @@
 
 struct EnLookNuts;
 
+typedef void (*EnLookNutsActionFunc)(struct EnLookNuts*, GlobalContext*);
+
+#define LOOKNUTS_LIMB_COUNT 11
+
 typedef struct EnLookNuts {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x14C];
+    /* 0x144 */ SkelAnime skelAnime;
+    ///* 0x188 */ char unk144[0x100];
+    /* 0x188 */ Vec3s limbDrawTbl[LOOKNUTS_LIMB_COUNT];
+    /* 0x1CA */ Vec3s transitionDrawTable[LOOKNUTS_LIMB_COUNT];
+    /* 0x20C */ EnLookNutsActionFunc actionFunc;
+    ///* 0x210 */ char unk210[0xA];
+    /* 0x210 */ s32 unk210;
+    /* 0x214 */ s16 unk214;
+    /* 0x21A */ u16 unk21A;
+    /* 0x21C */ s16 unk21C;
+    /* 0x21E */ s16 switchFlag;
+    /* 0x220 */ s16 paramsCopy2;
+    /* 0x222 */ s16 unk222;
+    /* 0x224 */ s16 unk224;
+    /* 0x226 */ s16 unk226;
+    /* 0x228 */ char unk228[0x1A];
+    /* 0x244 */ ColliderCylinder collider;
+    ///* 0x144 */ char unk144[0x14C];
 } EnLookNuts; // size = 0x290
 
 extern const ActorInit En_Look_Nuts_InitVars;
