31cffd814b3a00f4ea86229474b5deafa35f8bd1
isghj8 <isghj8@gmail.com>
2021-09-05 22:47:23 -0700

    EnPoSisters: rough start

--- src/overlays/actors/ovl_En_Po_Sisters/z_en_po_sisters.c
+++ src/overlays/actors/ovl_En_Po_Sisters/z_en_po_sisters.c
@@ -5,6 +5,7 @@
 #define THIS ((EnPoSisters*)thisx)
 
 void EnPoSisters_Init(Actor* thisx, GlobalContext* globalCtx);
+//void EnPoSisters_Init(EnPoSisters* this, GlobalContext* globalCtx);
 void EnPoSisters_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnPoSisters_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnPoSisters_Draw(Actor* thisx, GlobalContext* globalCtx);
@@ -26,7 +27,30 @@ void func_80B1BF2C(EnPoSisters* this, GlobalContext* globalCtx);
 void func_80B1C0A4(EnPoSisters* this, GlobalContext* globalCtx);
 void func_80B1C340(EnPoSisters* this, GlobalContext* globalCtx);
 
-#if 0
+// new
+void func_80B1A768(EnPoSisters* this, GlobalContext* globalCtx);
+void func_80B1A894(EnPoSisters* this, GlobalContext* globalCtx);
+void func_80B1A9B0(EnPoSisters* this, GlobalContext* globalCtx);
+void func_80B1AA88(EnPoSisters* this);
+void func_80B1AB5C(EnPoSisters* this);
+void func_80B1AC40(EnPoSisters* this);
+void func_80B1AE28(EnPoSisters* this);
+void func_80B1AF8C(EnPoSisters* this);
+void func_80B1B0E0(EnPoSisters* this);
+void func_80B1B280(EnPoSisters* this);
+void func_80B1B3A8(EnPoSisters* this);
+void func_80B1B5B4(EnPoSisters* this);
+void func_80B1B70C(EnPoSisters* this);
+void func_80B1BA3C(EnPoSisters* this);
+void func_80B1BC4C(EnPoSisters* this);
+void func_80B1BE4C(EnPoSisters* this);
+void func_80B1C030(EnPoSisters* this);
+void func_80B1C2E8(EnPoSisters* this);
+// might be s32 return
+void func_80B1C974(EnPoSisters* this);
+
+
+
 const ActorInit En_Po_Sisters_InitVars = {
     ACTOR_EN_PO_SISTERS,
     ACTORCAT_ENEMY,
@@ -91,13 +115,6 @@ static InitChainEntry D_80B1DAC4[] = {
     ICHAIN_F32(targetArrowOffset, 6000, ICHAIN_STOP),
 };
 
-#endif
-
-extern ColliderCylinderInit D_80B1DA70;
-extern CollisionCheckInfoInit D_80B1DA9C;
-extern DamageTable D_80B1DAA4;
-extern InitChainEntry D_80B1DAC4[];
-
 extern UNK_TYPE D_06000114;
 extern UNK_TYPE D_060008C0;
 extern UNK_TYPE D_06000A54;
@@ -107,21 +124,185 @@ extern UNK_TYPE D_060014CC;
 extern UNK_TYPE D_060027B0;
 extern UNK_TYPE D_060046E0;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/EnPoSisters_Init.s")
+extern SkeletonHeader D_060065C8;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/EnPoSisters_Destroy.s")
+#ifdef NON_MATCHING
+void EnPoSisters_Init(Actor* thisx, GlobalContext *globalCtx) {
+    EnPoSisters *this = THIS;
+    u8 sisType;
+
+    Actor_ProcessInitChain((Actor *) this, D_80B1DAC4);
+    ActorShape_Init(&this->actor.shape, 0.0f, (void (*)(struct Actor *actor, struct Lights *mapper, GlobalContext *globalCtx)) func_800B3FC0, 50.0f);
+    SkelAnime_Init(globalCtx, &this->skelAnime, &D_060065C8, (AnimationHeader *) &D_060014CC, this->limbDrawTbl, this->transitionDrawTable, 0xC);
+    this->unkColor226.r = 0xFF;
+    this->unkColor226.g = 0xFF;
+    this->unkColor226.b = 0xD2;
+    this->unkColor226.a = 0xFF;
+    //temp_a2 = &this->lightInfo;
+    //sp34 = temp_a2;
+    this->lightNode = LightContext_InsertLight(globalCtx, &globalCtx->lightCtx, &this->lightInfo);
+    Lights_PointGlowSetInfo(&this->lightInfo, (s16) (s32) this->actor.home.pos.x, (s16) (s32) this->actor.home.pos.y, (s16) (s32) this->actor.home.pos.z, 0, 0, 0, 0);
+    Collider_InitAndSetCylinder(globalCtx, &this->collider, (Actor *) this, &D_80B1DA70);
+    CollisionCheck_SetInfo(&this->actor.colChkInfo, &D_80B1DAA4, &D_80B1DA9C);
+
+    // issues here: vanilla wants to load params into three separate registers...
+    // last time I ran into that issue the answer was using this and thisx together, but three?
+
+    //sisType = ((s32) this->actor.params >> 8) & 3;
+    //sisType = ((s32) thisx->params >> 8) & 3;
+
+    this->sisterType = (this->actor.params >> 8) & 3;
+    //this->sisterType = sisType;
+    //this->actor.hintId = sisType + 0x50;
+    //this->actor.hintId = this->sisterType + 0x50;
+    this->actor.hintId = ((this->actor.params >> 8) & 3) + 0x50;
+    if (1) {}
+    //this->unk18D = (this->actor.params >> 0xA) & 3;
+    this->unk18D = (thisx->params >> 0xA) & 3;
+    this->unk18E = 0x20;
+    this->unk18F = 0x14;
+    this->unk190 = 1;
+    this->unk191 = 0x20;
+    this->actor.flags &= -0x2;
+    this->unk2EC = 110.0f;
+    if ((this->actor.params & 0x1000) != 0) {
+    //if ((thisx->params & 0x1000) != 0) {
+        func_80B1AA88(this);
+    } else if (this->sisterType == 0) {
+        if (this->unk18D == 0) {
+            this->actor.colChkInfo.health = 8;
+            this->collider.info.toucher.damage = 0x10;
+            this->collider.base.ocFlags1 = 9;
+            func_80B1BCF0(this, globalCtx);
+            func_80B1C2E8(this);
+        } else {
+            this->collider.info.elemType = 4;
+            this->actor.flags &= -0x4201;
+            this->collider.info.bumper.dmgFlags |= 0x40001;
+            this->collider.base.ocFlags1 = 0;
+            func_80B1BE4C(this);
+        }
+    } else {
+        func_80B1C2E8(this);
+    }
+    this->actor.params &= 0xFF;
+}
+#else
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/EnPoSisters_Init.s")
+#endif
+
+void EnPoSisters_Destroy(Actor *thisx, GlobalContext *globalCtx) {
+    EnPoSisters* this = THIS;
+
+    LightContext_RemoveLight(globalCtx, &globalCtx->lightCtx, this->lightNode);
+    Collider_DestroyCylinder(globalCtx, &this->collider);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/func_80B1A648.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/func_80B1A768.s")
+// this control flow looks like !A || B but why does it load 2ec twice?
+#ifdef NON_MATCHING
+void func_80B1A768(EnPoSisters *this, GlobalContext *globalCtx) {
+    f32 sp20;
+    Player *player = PLAYER;
+    //u8 temp_v0;
 
+    if (this->unk18D == 0 || this->actionFunc != func_80B1B444) {
+    //temp_v0 = this->unk18D;
+    // reverse
+    ///* 
+        if (!(player->swordState == 0 || player->swordAnimation  >= 0x1E) 
+          || (player->actor.world.pos.y - player->actor.floorHeight < 1.0f)) {
+            Math_StepToF(&this->unk2EC, 170.0f, 10.0f);
+        } else {
+            Math_StepToF(&this->unk2EC, 110.0f, 3.0f);
+            sp20 = this->unk2EC;
+        }
+
+    //  */
+
+        //if ((player->swordState == 0) || (player->swordAnimation  >= 0x1E)) {
+            //if ((player->actor.world.pos.y - player->actor.floorHeight) < 1.0f) {
+                //Math_StepToF(&this->unk2EC, 110.0f, 3.0f);
+            //} else {
+        //block_6:
+                //Math_StepToF(&this->unk2EC, 170.0f, 10.0f);
+            //}
+        //} else {
+            //goto block_6;
+        //}
+    } else if (this->unk18D != 0) {
+        sp20 = this->actor.parent->xzDistToPlayer;
+    }
+    this->actor.world.pos.x = (Math_SinS((s16) (this->actor.shape.rot.y + 0x8000)) * sp20) + player->actor.world.pos.x;
+    this->actor.world.pos.z = (Math_CosS((s16) (this->actor.shape.rot.y + 0x8000)) * sp20) + player->actor.world.pos.z;
+}
+#else
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/func_80B1A768.s")
+#endif
+
+// decr is being stupid
+#ifdef NON_MATCHING
+void func_80B1A894(EnPoSisters *this, GlobalContext *globalCtx) {
+    f32 sp24;
+    //u8 temp_t8;
+    //u8 temp_v0;
+    //u8 phi_v0;
+    //u8 pad;
+    Player* player = PLAYER;
+
+    Math_ApproachF(&this->actor.world.pos.y, player->actor.world.pos.y + 5.0f, 0.5f, 3.0f);
+
+    // this section looks like a timer countdown with DECR but wont match
+    //temp_v0 = this->unk18E;
+    //phi_v0 = temp_v0;
+    if (this->unk18E == 0) {
+        this->unk18E = 0x20;
+        //phi_v0 = (u8)0x20U & 0xFF;
+    }
+    //phi_v1 = phi_v0;
+    if (this->unk18E != 0) {
+        ////temp_t8 = phi_v0 - 1;
+        this->unk18E--;
+        ////phi_v1 = temp_t8 & 0xFF;
+    }
+    sp24 = Math_SinS(((this->unk18E << 0x1B) >> 0x10));
+    //sp24 = Math_SinS(((DECR(this->unk18E) << 0x1B) >> 0x10));
+
+    // the branching here is a bit odd, postdecr without DECR's zero check is the cloesest
+    //sp24 = Math_SinS(((this->unk18E-- << 0x1B) >> 0x10));
+    this->actor.world.pos.y += (2.0f + (0.5f * Rand_ZeroOne())) * sp24;
+    if (this->unkColor226.a == 0xFF) {
+        if ((this->actionFunc != func_80B1B168 ) && (this->actionFunc != func_80B1B020 )) {
+            if (this->actionFunc == func_80B1B628 ) {
+                func_800B9010((Actor *) this, NA_SE_EN_PO_AWAY - SFX_FLAG);
+                return;
+            }
+            func_800B9010((Actor *) this, NA_SE_EN_PO_FLY - SFX_FLAG);
+        }
+    }
+}
+#else
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/func_80B1A894.s")
+#endif
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/func_80B1A9B0.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/func_80B1AA88.s")
+void func_80B1AA88(EnPoSisters *this) {
+    SkelAnime_ChangeAnimTransitionRepeat(&this->skelAnime, &D_060014CC, -3.0f);
+    this->actor.speedXZ = 0.0f;
+    this->unk192 = Rand_S16Offset(100, 50);
+    this->actionFunc = func_80B1AAE8;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/func_80B1AAE8.s")
+void func_80B1AAE8(EnPoSisters *this, GlobalContext *globalCtx) {
+
+    SkelAnime_FrameUpdateMatrix(&this->skelAnime);
+    if (DECR(this->unk192) == 0) {
+        this->unk192 = Rand_S16Offset(100, 50);
+        Audio_PlayActorSound2((Actor *) this, NA_SE_EN_PO_LAUGH2);
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Po_Sisters/func_80B1AB5C.s")
 
--- src/overlays/actors/ovl_En_Po_Sisters/z_en_po_sisters.h
+++ src/overlays/actors/ovl_En_Po_Sisters/z_en_po_sisters.h
@@ -7,11 +7,37 @@ struct EnPoSisters;
 
 typedef void (*EnPoSistersActionFunc)(struct EnPoSisters*, GlobalContext*);
 
+#define PO_SISTER_LIMB_COUNT 0xC
+
 typedef struct EnPoSisters {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x44];
+    /* 0x0144 */ SkelAnime skelAnime;
     /* 0x0188 */ EnPoSistersActionFunc actionFunc;
-    /* 0x018C */ char unk_18C[0x20C];
+    /* 0x018C */ u8 sisterType;
+    /* 0x018D */ u8 unk18D; // 0xA shifted type?
+    /* 0x018E */ u8 unk18E;
+    /* 0x018F */ u8 unk18F;
+    /* 0x0190 */ u8 unk190;
+    /* 0x0191 */ u8 unk191;
+    /* 0x0192 */ s16 unk192;
+    /* 0x0194 */ s16 unk194; // timer?
+    /* 0x0196 */ Vec3s limbDrawTbl[PO_SISTER_LIMB_COUNT];
+    /* 0x01DE */ Vec3s transitionDrawTable[PO_SISTER_LIMB_COUNT];
+    /* 0x0226 */ Color_RGBA8 unkColor226; 
+    /* 0x022C */ f32 unk22C; 
+    /* 0x0230 */ f32 unk230; 
+    /* 0x0234 */ f32 unk234; 
+    /* 0x0238 */ char unk238[0x4]; 
+    /* 0x023C */ f32 unk23C; // read but not set? what 
+    /* 0x0240 */ char unk240[0xAC]; 
+    /* 0x02EC */ f32 unk2EC; 
+    /* 0x02F0 */ f32 unk2F0; 
+    /* 0x02F4 */ f32 unk2F4; 
+    /* 0x02F8 */ LightNode* lightNode; 
+    /* 0x02FC */ LightInfo lightInfo; 
+    ///* 0x0300 */ char unk300[0xC]; 
+    /* 0x030C */ ColliderCylinder collider; 
+    /* 0x0358 */ char unk358[0x20C];
 } EnPoSisters; // size = 0x398
 
 extern const ActorInit En_Po_Sisters_InitVars;
