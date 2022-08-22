41a8bd645e3b2ec30c2504ed9f2f274a416549a3
Isghj5 <isghj5@gmail.com>
2022-08-22 15:01:00 -0700

    Grog: forgot nwc needs this struct to stay intact, removing something unused shifted the stack

--- src/overlays/actors/ovl_En_Hs/z_en_hs.h
+++ src/overlays/actors/ovl_En_Hs/z_en_hs.h
@@ -29,7 +29,7 @@ typedef struct EnHs {
     /* 0x01D4 */ Vec3s jointTable[OBJECT_HS_LIMB_MAX];
     /* 0x0234 */ Vec3s morphTable[OBJECT_HS_LIMB_MAX];
     /* 0x0294 */ Vec3s headRot;
-    ///* 0x029A */ Vec3s unusedRot; // probably not chest, as chest is same limb as lower body
+    /* 0x029A */ Vec3s unusedRot; // probably not chest, as chest is same limb as lower body
     /* 0x02A0 */ u16 stateFlags; // 0-4
     /* 0x02A2 */ s16 stateTimer; // reused for different actionFunc
     /* 0x02A4 */ Vec3f nwcPos[20]; // actual chick position are even values, odd values seem to be extra values for smoother chain
