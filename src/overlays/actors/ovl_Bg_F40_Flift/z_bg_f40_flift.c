4ae49514285374c87126ea53ff5c6c484e5d9586
Isghj5 <isghj5@gmail.com>
2022-10-30 23:47:23 -0700

    Cube: Cube

--- src/overlays/actors/ovl_Bg_F40_Flift/z_bg_f40_flift.c
+++ src/overlays/actors/ovl_Bg_F40_Flift/z_bg_f40_flift.c
@@ -5,7 +5,7 @@
  */
 
 #include "z_bg_f40_flift.h"
-#include "objects/object_f40_obj/object_f40_obj.h"
+#include "objects/gameplay_keep/gameplay_keep.h"
 
 #define FLAGS (ACTOR_FLAG_10)
 
@@ -16,14 +16,14 @@ void BgF40Flift_Destroy(Actor* thisx, PlayState* play);
 void BgF40Flift_Update(Actor* thisx, PlayState* play);
 void BgF40Flift_Draw(Actor* thisx, PlayState* play);
 
-void func_808D75F0(BgF40Flift* this, PlayState* play);
-void func_808D7714(BgF40Flift* this, PlayState* play);
+void BgF40Flift_Idle(BgF40Flift* this, PlayState* play);
+void BgF40Flift_Elevate(BgF40Flift* this, PlayState* play);
 
 ActorInit Bg_F40_Flift_InitVars = {
     ACTOR_BG_F40_FLIFT,
     ACTORCAT_BG,
     FLAGS,
-    OBJECT_F40_OBJ,
+    GAMEPLAY_KEEP,
     sizeof(BgF40Flift),
     (ActorFunc)BgF40Flift_Init,
     (ActorFunc)BgF40Flift_Destroy,
@@ -34,17 +34,20 @@ ActorInit Bg_F40_Flift_InitVars = {
 static InitChainEntry sInitChain[] = {
     ICHAIN_F32(uncullZoneScale, 400, ICHAIN_CONTINUE),
     ICHAIN_F32(uncullZoneForward, 5000, ICHAIN_CONTINUE),
-    ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP),
+    ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP), // scale
 };
+
 
 void BgF40Flift_Init(Actor* thisx, PlayState* play) {
     BgF40Flift* this = THIS;
 
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
     DynaPolyActor_Init(&this->dyna, 1);
-    DynaPolyActor_LoadMesh(play, &this->dyna, &object_f40_obj_Colheader_004240);
+    //DynaPolyActor_LoadMesh(play, &this->dyna, &object_f40_obj_Colheader_004240); // vanilla
+    DynaPolyActor_LoadMesh(play, &this->dyna, &gameplay_keep_Colheader_05A270); // not sure if this is what we want
     this->dyna.actor.params = 1;
-    this->actionFunc = func_808D75F0;
+    this->actionFunc = BgF40Flift_Idle;
+    this->dyna.actor.home.pos.y += 5;
 }
 
 void BgF40Flift_Destroy(Actor* thisx, PlayState* play) {
@@ -53,11 +56,12 @@ void BgF40Flift_Destroy(Actor* thisx, PlayState* play) {
     DynaPoly_DeleteBgActor(play, &play->colCtx.dyna, this->dyna.bgId);
 }
 
-void func_808D75F0(BgF40Flift* this, PlayState* play) {
+void BgF40Flift_Idle(BgF40Flift* this, PlayState* play) {
     if (((this->dyna.actor.params == 1) && (DynaPolyActor_IsInRidingMovingState(&this->dyna))) ||
         ((this->dyna.actor.params == -1) && (!DynaPolyActor_IsInRidingMovingState(&this->dyna)))) {
+        //this->timer = 96;
         this->timer = 96;
-        this->actionFunc = func_808D7714;
+        this->actionFunc = BgF40Flift_Elevate;
     } else {
         if (this->timer == 0) {
             this->timer = 48;
@@ -65,6 +69,7 @@ void func_808D75F0(BgF40Flift* this, PlayState* play) {
         this->timer--;
         if (this->dyna.actor.params == 1) {
             this->dyna.actor.world.pos.y = (sin_rad(this->timer * (M_PI / 24.0f)) * 5.0f) + this->dyna.actor.home.pos.y;
+            this->dyna.actor.shape.rot.y = this->dyna.actor.world.rot.y += 50;
         } else {
             this->dyna.actor.world.pos.y =
                 (sin_rad(this->timer * (M_PI / 24.0f)) * 5.0f) + (926.8f + this->dyna.actor.home.pos.y);
@@ -72,15 +77,17 @@ void func_808D75F0(BgF40Flift* this, PlayState* play) {
     }
 }
 
-void func_808D7714(BgF40Flift* this, PlayState* play) {
+void BgF40Flift_Elevate(BgF40Flift* this, PlayState* play) {
     if (this->timer != 0) {
         this->timer--;
         this->dyna.actor.world.pos.y =
-            (((cos_rad(this->timer * (M_PI / 96.0f)) * this->dyna.actor.params) + 1.0f) * 463.4f) +
+            //(((cos_rad(this->timer * (M_PI / 96.0f)) * this->dyna.actor.params) + 1.0f) * 463.4f) +
+            (((cos_rad(this->timer * (M_PI / 96.0f)) * this->dyna.actor.params) + 1.0f) * 163.4f) +
             this->dyna.actor.home.pos.y;
     } else {
+        // decend
         this->dyna.actor.params = -this->dyna.actor.params;
-        this->actionFunc = func_808D75F0;
+        this->actionFunc = BgF40Flift_Idle;
     }
 }
 
@@ -91,5 +98,6 @@ void BgF40Flift_Update(Actor* thisx, PlayState* play) {
 }
 
 void BgF40Flift_Draw(Actor* thisx, PlayState* play) {
-    Gfx_DrawDListOpa(play, object_f40_obj_DL_004038);
+    //Gfx_DrawDListOpa(play, object_f40_obj_DL_004038);
+    Gfx_DrawDListOpa(play, gameplay_keep_DL_05A0B0);
 }
