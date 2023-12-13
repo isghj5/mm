a5ea2b0be29d18a819230c16c4748874cb78b25f
engineer124 <47598039+engineer124@users.noreply.github.com>
2023-12-13 15:16:24 +1100

    Use more `TRUNCF_BINANG` for f32 -> s16 casts (#1503)
    * TRUNCF_BINANG
    
    * brackets
    
    * extra cast

--- src/overlays/actors/ovl_Bg_F40_Swlift/z_bg_f40_swlift.c
+++ src/overlays/actors/ovl_Bg_F40_Swlift/z_bg_f40_swlift.c
@@ -16,8 +16,8 @@ void BgF40Swlift_Destroy(Actor* thisx, PlayState* play);
 void BgF40Swlift_Update(Actor* thisx, PlayState* play2);
 void BgF40Swlift_Draw(Actor* thisx, PlayState* play);
 
-static s32 sSwitchFlags[4] = { 0xFF, 0xFF, 0xFF, 0xFF };
-static s32 sHeights[4];
+//static s32 sSwitchFlags[4] = { 0xFF, 0xFF, 0xFF, 0xFF };
+//static s32 sHeights[4];
 
 ActorInit Bg_F40_Swlift_InitVars = {
     /**/ ACTOR_BG_F40_SWLIFT,
@@ -39,26 +39,29 @@ static InitChainEntry sInitChain[] = {
 
 void BgF40Swlift_Init(Actor* thisx, PlayState* play) {
     BgF40Swlift* this = THIS;
-    s32 index;
-    s32 pad;
+    //s32 index;
+    //s32 pad;
 
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
     DynaPolyActor_Init(&this->dyna, DYNA_TRANSFORM_POS);
-    index = BG_F40_SWLIFT_GET_INDEX(thisx);
-    if ((index < 0) || (index >= 5)) { //! @bug An index greater than 3 will cause an out of bounds array access.
-        Actor_Kill(&this->dyna.actor);
-        return;
-    }
+    //index = BG_F40_SWLIFT_GET_INDEX(thisx);
+    //if ((index < 0) || (index >= 5)) { //! @bug An index greater than 3 will cause an out of bounds array access.
+        //Actor_Kill(&this->dyna.actor);
+        //return;
+    //}
 
-    sHeights[index] = this->dyna.actor.world.pos.y;
-    sSwitchFlags[index] = BG_F40_SWLIFT_GET_SWITCH_FLAG(thisx);
-    if (index != 0) {
-        Actor_Kill(&this->dyna.actor);
-        return;
-    }
+    //sHeights[index] = this->dyna.actor.world.pos.y;
+    //sSwitchFlags[index] = BG_F40_SWLIFT_GET_SWITCH_FLAG(thisx);
+
+    // my guess is they wanted to use a switch flag to move the platform to change dungeon layout
+    //if (index != 0) {
+        //Actor_Kill(&this->dyna.actor);
+        //return;
+    //}
 
     DynaPolyActor_LoadMesh(play, &this->dyna, &gStoneTowerVerticallyOscillatingPlatformCol);
-    this->dyna.actor.params = 0;
+
+    //this->dyna.actor.params = 0; // resetting??
 }
 
 void BgF40Swlift_Destroy(Actor* thisx, PlayState* play) {
@@ -72,41 +75,42 @@ void BgF40Swlift_Update(Actor* thisx, PlayState* play2) {
     BgF40Swlift* this = THIS;
     s32 i;
 
-    for (i = 1; i < ARRAY_COUNT(sSwitchFlags); i++) {
-        if ((sSwitchFlags[i] == 0xFF) || !Flags_GetSwitch(play, sSwitchFlags[i])) {
-            break;
-        }
-    }
+    //for (i = 1; i < ARRAY_COUNT(sSwitchFlags); i++) {
+        //if ((sSwitchFlags[i] == 0xFF) || !Flags_GetSwitch(play, sSwitchFlags[i])) {
+            //break;
+        //}
+    //}
 
-    i--;
-    if (i != this->dyna.actor.params) {
-        f32 heightOffset;
+    //i--;
+    //if (i != this->dyna.actor.params) {
+        //f32 heightOffset;
 
-        this->dyna.actor.params = -1;
-        heightOffset = (sHeights[i] - this->dyna.actor.world.pos.y) * 0.1f;
-        if (heightOffset > 0.0f) {
-            heightOffset = CLAMP(heightOffset, 0.5f, 15.0f);
-        } else {
-            heightOffset = CLAMP(heightOffset, -15.0f, -0.5f);
-        }
+        //this->dyna.actor.params = -1;
+        //heightOffset = (sHeights[i] - this->dyna.actor.world.pos.y) * 0.1f;
+        //if (heightOffset > 0.0f) {
+            //heightOffset = CLAMP(heightOffset, 0.5f, 15.0f);
+        //} else {
+            //heightOffset = CLAMP(heightOffset, -15.0f, -0.5f);
+        //}
 
-        if (Math_StepToF(&this->dyna.actor.speed, heightOffset, 1.0f) && (fabsf(heightOffset) <= 0.5f)) {
-            if (Math_StepToF(&this->dyna.actor.world.pos.y, sHeights[i], fabsf(this->dyna.actor.speed))) {
-                this->dyna.actor.params = i;
-                this->timer = 48;
-                this->dyna.actor.speed = 0.0f;
-            }
-        } else {
-            this->dyna.actor.world.pos.y += this->dyna.actor.speed;
-        }
-    } else {
+        //if (Math_StepToF(&this->dyna.actor.speed, heightOffset, 1.0f) && (fabsf(heightOffset) <= 0.5f)) {
+            //if (Math_StepToF(&this->dyna.actor.world.pos.y, sHeights[i], fabsf(this->dyna.actor.speed))) {
+                //this->dyna.actor.params = i;
+                //this->timer = 48;
+                //this->dyna.actor.speed = 0.0f;
+            //}
+        //} else {
+            //this->dyna.actor.world.pos.y += this->dyna.actor.speed;
+        //}
+    //} else {
         if (this->timer == 0) {
             this->timer = 48;
         }
         this->timer--;
-        this->dyna.actor.world.pos.y =
-            sHeights[this->dyna.actor.params] + (Math_SinF(this->timer * (M_PI / 24.0f)) * 5.0f);
-    }
+        //this->dyna.actor.world.pos.y =
+            //sHeights[this->dyna.actor.params] + (Math_SinF(this->timer * (M_PI / 24.0f)) * 5.0f);
+        this->dyna.actor.world.pos.y = this->dyna.actor.home.pos.y + (Math_SinF(this->timer * (M_PI / 24.0f)) * 5.0f);
+    //}
 }
 
 void BgF40Swlift_Draw(Actor* thisx, PlayState* play) {
